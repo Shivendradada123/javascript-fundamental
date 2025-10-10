15eeaf588141b60f740b28b65b73ff73c9e82db4
Shivedra123 <shivendrasaket2580@gmail.com>
2025-10-10 08:31:08 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,19 @@
+const array = [1, 3, 5, 7, 7, 8, 9 ,323]
+
+console.log(array);
+
+console.log(array[5]);
+array[45] = 65;  // array me oder matter karta hai 
+console.log(array[45]);
+
+
+
+console.log( "this is a length", array.length);  
+
+// 0 se 4  values hai
+
+// 5 se 44  empty (koye value nhi)
+
+// 45  value = 65
+//  Total indexes = 46
+  
