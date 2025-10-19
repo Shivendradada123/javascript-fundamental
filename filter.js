c573953bd824493b1aa5a7427c240b7e1b34b433
Shivedra123 <shivendrasaket2580@gmail.com>
2025-10-19 21:11:12 +0530

    filter

--- filter.js
+++ filter.js
@@ -1,11 +1,17 @@
-const name = ["shivendra", "aditya", "code"];
+// const name = ["shivendra", "aditya", "code"];
  
-const filter = name.filter((Element) => Element.length > 5);
+// const filter = name.filter((Element) => Element.length > 5);
 
-console.log(filter);     
+// console.log(filter);     
 
 
 
 // sencod example 
 
 const number = [1,3,4,5,6,7,5,];
+
+const num = number.filter(number => number < 5);
+
+console.log(num);
+
+
