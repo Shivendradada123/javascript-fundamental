88cfbae6ef784bf91cb1e4ece58f6e28982cd056
Shivedra123 <shivendrasaket2580@gmail.com>
2025-10-19 21:10:51 +0530

    filter and fill in array

---
+++ fill.js
@@ -0,0 +1,9 @@
+const arry = [1,2,3,4,5,6,7,8,9,0];
+
+const arry2 = arry.fill(4,4,8);
+
+console.log(arry2);
+
+//  fill funtion jo hai jaise ki fill me (4) = suru me hai uska matlab 4 jo value hai usse suru karo 
+// 4 jo hai second me uska matalb 4 index se us value se suru karo
+// 8 matlab end index 8 me ! leken index 8 inclue nhi hota hai 
---
+++ filter.js
@@ -0,0 +1,11 @@
+const name = ["shivendra", "aditya", "code"];
+ 
+const filter = name.filter((Element) => Element.length > 5);
+
+console.log(filter);     
+
+
+
+// sencod example 
+
+const number = [1,3,4,5,6,7,5,];
