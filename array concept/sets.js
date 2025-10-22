d8c3867535dfdfd1dc9e1eedf0f5b8b25c5dce68
Shivedra123 <shivendrasaket2580@gmail.com>
2025-10-22 21:32:20 +0530

    ////

--- array concept/sets.js
+++ array concept/sets.js
@@ -1,7 +1,6 @@
 const array = [1,22,22,11,11,1,3,4,5,6,7,666,444,444]
 const sets = [...new Set(array)];
-console.log(sets)
+console.log(sets)   
 
 
-
-
+console
---
+++ console/clear.js
@@ -0,0 +1,11 @@
+
+
+console.clear();
+
+const num = 10;
+
+for (let i = 1; i <= num; i++) {
+  console.log(i);
+}
+
+
