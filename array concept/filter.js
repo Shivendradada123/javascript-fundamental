8b1a73c28ced26b93ecd1c2890290da864e4672f
Shivedra123 <shivendrasaket2580@gmail.com>
2025-10-25 07:38:21 +0530

    block scope

--- array concept/filter.js
+++ array concept/filter.js
@@ -8,10 +8,14 @@
 
 // sencod example 
 
-const number = [1,3,4,5,6,7,5,];
+// const number = [1,3,4,5,6,7,5,];
 
-const num = number.filter(number => number < 5);
+// const num = number.filter(number => number < 5);
 
-console.log(num);
+// console.log(num);
 
 
+
+
+
+console.log("GOOD NIGHT ADITYA JI 🥰")
---
+++ array concept/let,var,const.js
@@ -0,0 +1,8 @@
+{ var varName = "Shivendra"; let myName = "Shivendra" ; const constName = "Shivendra"}
+
+
+console.log(varName)
+console.log(myName)
+console.log(constName)
+
+
