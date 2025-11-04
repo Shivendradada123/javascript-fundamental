13e2f1ad90aabd3ffd3c04d988d074e20effb968
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-04 20:24:34 +0530

    question?solve

---
+++ freecode_question_solve/condtion_type.js
@@ -0,0 +1,19 @@
+// question
+
+// // 1. trueOrFalse should be a function
+// Passed:2. trueOrFalse(true) should return a string
+// Passed:3. trueOrFalse(false) should return a string
+// Passed:4. trueOrFalse(true) should return the string Yes, that was true
+// Passed:5. trueOrFalse(false) should return the string No, that was false
+
+
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue){
+  return "Yes, that was true";
+} 
+  return "No, that was false";
+}
+trueOrFalse(true);
+trueOrFalse(false);
