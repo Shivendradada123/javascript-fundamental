7f4af7d72b8ae49db6ce7ff4888840e4f8900b46
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-06 19:38:09 +0530

    Understanding Case Sensitivity in Variables

---
+++ freecode_question_solve/Declare String Variables.js
@@ -0,0 +1,8 @@
+// 1. myFirstName should be a string with at least one character in it.
+// Passed:2. myLastName should be a string with at least one character in it.
+
+
+var myFirstName;
+var myLastName;
+myFirstName = "shivendra"
+myLastName = "saket"
---
+++ freecode_question_solve/Understanding Uninitialized Variables.js
@@ -0,0 +1,16 @@
+// Passed:1. a should be defined and have a final value of 6.
+// Passed:2. b should be defined and have a final value of 15.
+// Passed:3. c should not contain undefined and should have a final value of the string I am a String!
+// Passed:4. You should not change code below the specified comment
+
+
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
