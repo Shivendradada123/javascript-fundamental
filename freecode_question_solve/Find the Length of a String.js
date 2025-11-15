e1e59f8215c799224e19b4f3ea393c28c0c1bff1
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-15 12:17:45 +0530

    length

---
+++ freecode_question_solve/Find the Length of a String.js
@@ -0,0 +1,16 @@
+// // Setup
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
+
+// // Only change code below this line
+//    const check = lastNameLength = lastName.length; // jaise ki lovelace ki leanght 8 hai to output 8 ayega
+
+
+// console.log(check)  //8
+
+
+
+
+const nam = "Shivendra"
+ 
+console.log(nam.length) //8
