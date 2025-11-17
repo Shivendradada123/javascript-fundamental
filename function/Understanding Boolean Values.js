0065ecb6e941e6ae3b152a8efb1eab65d10a7eef
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-17 21:54:38 +0530

    function check a code

---
+++ function/Understanding Boolean Values.js
@@ -0,0 +1,14 @@
+function welcomeToBooleans() {
+  
+
+  return "Shivendra"; 
+
+
+}
+
+const fun = welcomeToBooleans();
+console.log(fun);
+
+
+
+// console.log(welcomeToBooleans());
---
+++ function/Use Conditional Logic with If Statement.js
@@ -0,0 +1,39 @@
+
+
+
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if (wasThatTrue){
+  return "Yes, that was true"
+} else {
+  return "No, that was false"
+}
+
+
+  // Only change code above this line
+
+}
+
+
+console.log(trueOrFalse(true))
+
+
+
+
+
+// second example 
+
+
+const name = "Shivendra"
+
+function namecheck(name){
+    if(name == "Shivendra"){
+        console.log("hi shivendra")
+    } else {
+        console.log("no you are not a shivendra")
+    }
+}
+
+
+namecheck(name);    // virrable ko yaha pass karte hai 
