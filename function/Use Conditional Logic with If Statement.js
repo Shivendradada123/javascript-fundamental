d69f316ee7d6bba7b4ddcdb831c4ac785ff7fea7
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-18 13:24:07 +0530

    create a function

--- function/Use Conditional Logic with If Statement.js
+++ function/Use Conditional Logic with If Statement.js
@@ -16,24 +16,78 @@ if (wasThatTrue){
 }
 
 
-console.log(trueOrFalse(true))
+// console.log(trueOrFalse(true))
 
 
 
 
 
-// second example 
+// // second example 
 
 
-const name = "Shivendra"
+// const name = "Shivendra"
 
-function namecheck(name){
-    if(name == "Shivendra"){
-        console.log("hi shivendra")
-    } else {
-        console.log("no you are not a shivendra")
-    }
+// function namecheck(name){
+//     if(name == "Shivendra"){
+//         console.log("hi shivendra")
+//     } else {
+//         console.log("no you are not a shivendra")
+//     }
+// }
+
+
+// namecheck(name);    // virrable ko yaha pass karte hai 
+
+
+
+
+
+
+
+// let nam = "chashma"
+
+// function chashma(){
+//  if(nam == "chashma") {
+//   return "reshma"
+//  } else{
+//   return "nigga"
+//  }
+// }
+
+// console.log(chashma(nam))
+
+// // second method
+
+// let i = "chashma" 
+
+// function chashmaa() {
+//   let i = ["Chashma"]
+//   i.shift();
+//   i.push("reshma")
+//   return i
+// }
+
+// console.log(chashmaa(i))
+
+
+
+
+
+let a = " "
+function namechange(){
+  let a = "aditya rawat"
+  a = "Aditya Rawat"
+
+  return a
+
 }
 
+console.log(namechange(a))
 
-namecheck(name);    // virrable ko yaha pass karte hai 
+
+
+
+
+
+
+
