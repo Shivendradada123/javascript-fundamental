7d02c8fcd7b9b3c6c950b9459b8a7e2851e78f83
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-19 15:47:15 +0530

    math md file;

---
+++ Logical opertor/Returning Boolean Values from Functions.js
@@ -0,0 +1,8 @@
+function isLess(a, b) {
+  
+   return a<=b    // yaha agar a ki value b se small aya eqal hai to true nhi to false
+  
+}
+
+ console.log(isLess(10, 15));
+ console.log(isLess(55, 10 ));
