d6dcfed4a9fb82e7acb22a803db230853bf46e51
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-21 10:54:37 +0530

    loop  md dile

--- conditions & loop/While Loops.js
+++ conditions & loop/While Loops.js
@@ -1,13 +1,24 @@
-const myArray = [];
-let i = 16;
+// const myArray = [];
+// let i = 16;
 
-while(i >= -8){
-    myArray.push(i);
-    i--;
+// while(i >= -8){
+//     myArray.push(i);
+//     i--;
+// }
+
+// console.log(myArray)
+
+
+// second
+
+let a = 0;
+
+while(a < 18){
+    
+    a += 2
 }
+console.log(a)     // 
+  
 
-console.log(myArray)
 
-
-// 
 
---
+++ conditions & loop/loop.md
@@ -0,0 +1,24 @@
+# While loop by Shivendra
+
+1. console loop ke andar 
+
+``` let a = 0;
+
+while(a < 18){
+    console.log(a)  // 0 to 18 print hoga
+    a += 2
+} ``` 
+
+
+
+# console loop  ke bahar
+
+1. while loop 
+
+``` let a = 0;
+
+while(a < 18){
+    
+    a += 2
+}
+console.log(a) ``` 
