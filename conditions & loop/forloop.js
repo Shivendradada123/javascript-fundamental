1f321acb5969f1ff0770c37ce7b025ac8435c91f
Shivedra123 <shivendrasaket2580@gmail.com>
2025-11-21 11:10:40 +0530

    Count Backwards

---
+++ conditions & loop/forloop.js
@@ -0,0 +1,7 @@
+let array = []
+
+ for(i = 0 ; i <= 16; i++){
+array.push(i)
+}
+
+console.log(array)  // 0 to 16 print hoga
---
+++ conditions & loop/while loop.js
@@ -0,0 +1,6 @@
+let a = 0;
+
+while(a < 18){
+    console.log(a)  // 0 to 18 print hoga
+    a += 2
+};
