9bb017d68518574b34214b735ef46ac2b6a46124
Shivedra123 <shivendrasaket2580@gmail.com>
2025-12-08 22:37:25 +0530

    tricky questions

--- daily_code_pretice/script.js
+++ daily_code_pretice/script.js
@@ -1,14 +1,31 @@
-let a = ""
+// let a = ""
 
-const value=()=>{
-    if(a === "Aditya"){
-        return "Hi "
-    }else{
-        return "niga"
-    }
-}
+// const value=()=>{
+//     if(a === "Aditya"){
+//         return "Hi "
+//     }else{
+//         return "niga"
+//     }
+// }
 
-console.log(value("Aditya"))
+// console.log(value("Aditya"))
+
+
+
+
+
+
+
+
+// console.log(a); // undefined
+// var a = 10;
+
+
+
+
+
+console.log(1 + "2" + 3); //
+console.log(1 + 2 + "3");
 
 
 
---
+++ trickyquestiion.md
@@ -0,0 +1,20 @@
+# Tricky Questions
+## 1st question
+
+1. question what is output this question
+```
+console.log(a);  // undefined
+var a = 10;
+```
+
+
+## second question
+1. Dheko agar Agar ek baar string aagayi, sab string ban jaata hai
+
+
+```
+console.log(1 + "2" + 3);   // "123"
+console.log(1 + 2 + "3");   // "33"
+```
+
+
