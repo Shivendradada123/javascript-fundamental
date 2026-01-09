4b4d1feb779e1c43c4139705d54c1e6ea97dfb46
Shivedra123 <shivendrasaket2580@gmail.com>
2026-01-09 21:45:27 +0530

    theme1,2,3

--- function/ss.js
+++ function/ss.js
@@ -1,19 +1,47 @@
-let a = 5;
+// let a = 5;
 
 
 
-if(a=0){
-  console.log(true)
-}else{
-  console.log(false)
-} 
+// if(a=0){
+//   console.log(true)
+// }else{
+//   console.log(false)
+// } 
 
 
 
 
 
-if(a==5){
-  console.log(true)
-}else{
-  console.log(false)
-}
+// if(a==5){
+//   console.log(true)
+// }else{
+//   console.log(false)
+// }``
+
+
+
+
+
+
+
+const theme1=()=>{
+  console.log('red')
+};
+const theme2=()=>{
+  console.log('green')
+};
+
+
+const allTheme=()=>{
+
+theme1();
+theme2();
+
+  console.log('NaviBlue')
+
+
+};
+
+allTheme();
+theme1();
+theme2()
