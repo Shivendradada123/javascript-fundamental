4795fda73489311c2c3abcdaefb622ee246692a0
Shivedra123 <shivendrasaket2580@gmail.com>
2026-01-22 20:11:57 +0530

    Eruda console

--- Logical opertor/Comparisons with the Logical Or Operator.js
+++ Logical opertor/Comparisons with the Logical Or Operator.js
@@ -1,14 +1,18 @@
-function testLogicalOr(val) {
-  // Only change code below this line
+// function testLogicalOr(val) {
+//   // Only change code below this line
 
-  if (val <10 || val > 80) {
-    return "Outside";
-  }
+//   if (val <10 || val > 80) {
+//     return "Outside";
+//   }
 
   
 
-  // Only change code above this line
-  return "Inside";
-}
+//   // Only change code above this line
+//   return "Inside";
+// }
 
-testLogicalOr(15);
+// testLogicalOr(15);
+
+
+
+javascript:(function () {     var script =  document.createElement('script');    script.src="//cdn.jsdelivr.net/npm/eruda";     document.body.appendChild(script);    script.onload = function () {         eruda.init()     } })()
